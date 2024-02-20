5aef9466c21057c1105f50a2c20fabe2fa4cea2b
prudhvi951 <107334256+prudhvi951@users.noreply.github.com>
2024-02-20 18:33:34 +0530

    Delete day18.py

--- day18.py
+++
@@ -1,9 +0,0 @@
-#divide apples to child
-#n,k = map(int,input().split())
-n = int(input())
-k =int(input())
-#print(k%n)
-while(k>n):#apples > child
-    k=k-n #apples - child equall distubition
-print(k)#print reamining                            
-    
