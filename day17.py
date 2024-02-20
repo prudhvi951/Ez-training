b26bbf37d4439c21841b0a73ec4c198292142db6
prudhvi951 <107334256+prudhvi951@users.noreply.github.com>
2024-02-20 18:32:40 +0530

    Delete day17.py

--- day17.py
+++
@@ -1,6 +0,0 @@
-#traingle validator problem sum of two sides of traingle must be greater than 3 side
-n,m,l = map(int,input().split(" "))
-if n+m>l and m+l>n and n+1>m:
-    print("Yes")
-else:
-    print("No")
